a198c7509f9cfa6a26babcacc8c90c10bc3df837
Alex E <teatime_dev@outlook.com>
2022-01-15 17:42:57 +1000

    hehe

--- index_files/test.js
+++ index_files/test.js
@@ -1,4 +1,4 @@
 function dontclick() {
     document.getElementById("dontclickelement").tagName = "h1";
-    document.getElementById("dontclickelement").innerHTML = "<iframe><body><h1>you just got iframe'd !!!!</h1></body></iframe>";
+    document.getElementById("dontclickelement").innerHTML = "<iframe src=\"horse.html\"></iframe>";
 }
