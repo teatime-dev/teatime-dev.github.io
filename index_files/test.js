9852cf0cdc6523741c0ffe99d75141abe3c6eeb0
Alexander <teatime_dev@outlook.com>
2022-07-27 23:23:03 +1000

    hehe bepos

--- index_files/test.js
+++ index_files/test.js
@@ -2,3 +2,18 @@ function dontclick() {
     document.getElementById("dontclickelement").tagName = "h1";
     document.getElementById("dontclickelement").innerHTML = "<iframe src=\"horse.html\"></iframe>";
 }
+
+
+// function amogus() {
+//     document.getElementById("horseimg").style="";
+//     amogusCounter--;
+//     if(amogusCounter>=0) {
+//         console.log("amogus");
+//         setTimeout(amogus,1000);
+//     } else {
+//         setTimeout(amogusSound,1000);
+//     };
+// }
+// amogusText2 = amogusText.split("\n");
+// amogusCounter = amogusText2.length-1;
+// amogus()
